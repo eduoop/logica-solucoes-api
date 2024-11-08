426d3fca21280902b4eb7fa66c0e21d57018bc05
Eduardo <eduardo.meneses@c2sglobal.com>
2024-11-08 16:10:18 -0300

    feat: add swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EnvService } from './env/env.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
-    const envService = app.get(EnvService)
+  const envService = app.get(EnvService)
 
-    const port = envService.get("PORT")
+  const port = envService.get("PORT")
 
-    await app.listen(port);
+  const config = new DocumentBuilder()
+    .setTitle('Lógica soluções API')
+    .setDescription('API para listagem e gerenciamento de usuários.')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port);
 }
 bootstrap();
